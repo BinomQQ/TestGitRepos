3e7fe2efc4074b1032bec72ad9b1fef91a89e59b
Binom <skbinom@gmail.com>
2018-05-09 12:05:16 +0300

    new file

---
+++ TestPythonGit.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("I'm in TestGitRepos\n")
+
+text = input("Please input text:\n")
+
+print("Your text:" + text)
